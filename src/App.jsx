a2adf2a6515039cc2048618b403fd1b91bc82c7a
naza625 <nazavillafanez@gmail.com>
2026-07-21 14:35:22 -0300

    agregue el maquetado del header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Header from "./components/Header"
+
 function App() {
-  
+    //aqui va a ir la logica del compnente
 
   return (
-   <h1>H</h1>
+    <>
+    {/*aqui va la zona del html*/}
+    <Header />
+    <h1>Hello, World!</h1>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="container my-4">
+            <h1 className="display-3 text-center">Practiva de react</h1>
+            <p className="lead text-center">Rect es una biblioteca de JavaScript para construir interfaces de usuario. <br /> Sirve para crear aplicaciones web con una experiencia de usuario fluida.</p>
+        </header>
+    );
+};
+
+export default Header;
